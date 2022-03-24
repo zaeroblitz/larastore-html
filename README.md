a61231c68f5737d4e582699088d3a7c1720933d8
Zaero Blitz <zaeroblitz@gmail.com>
2022-03-24 22:43:23 +0700

    add navbar

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# BWAStore
+# LaraStore
---
+++ scripts/navbar-scroll.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(document).scroll(function () {
+    let $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop > $nav.height());
+  });
+});
